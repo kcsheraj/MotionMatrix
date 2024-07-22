1d063ecc689b6716fc9f74163202d8a8a710865c
Sheraj Kc <88295555+kcsheraj@users.noreply.github.com>
2024-07-21 23:45:09 -0400

    update cors.

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,9 +10,22 @@ require("dotenv").config({ path: "api/.env" }); // Load environment variables
 
 const app = express();
 app.use(express.json());
+const allowedOrigins = [
+  "https://motion-matrix-frontend.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: "https://motion-matrix-frontend.vercel.app", // replace with your actual frontend URL
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // if you need to handle cookies
   })
